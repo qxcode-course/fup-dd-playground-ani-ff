a264061dc87301fad268db062d8c7a37766c8219
Anne <anne.freire@alu.ufc.br>
2026-03-10 18:31:57 +0000

    Atividades

---
+++ sandbox/00_rascunhos/primeiro codigo/draft.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() { // func - função
+    var num1, num2 float64 // variavel
+    fmt.Scan(&num1, &num2) // lendo e escrevendo em num1 // scan ler e quer saber onde colocar o valor
+    fmt.Println(num1 + num2,) // println (ln) é pq quebra a linha
+    fmt.Println(1)
+}
