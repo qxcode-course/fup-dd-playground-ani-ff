f3bad61831ae1ddba243f5f15cc9965603610dc1
Anne <anne.freire@alu.ufc.br>
2026-03-30 17:58:33 +0000

    Atividade "operações"

--- fub/operacoes/go/draft.go
+++ fub/operacoes/go/draft.go
@@ -1,5 +1,18 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var a, b int
+    fmt.Scan(&a, &b)
+
+    soma := a + b
+    subtracao := a - b
+    multiplicacao := a * b
+    divisao := float64(a) / float64(b)
+    resto := a % b
+
+    fmt.Println(soma)
+    fmt.Println(subtracao)
+    fmt.Println(multiplicacao)
+    fmt.Printf("%.2f\n", divisao)
+    fmt.Println(resto)
 }
