28de9387430bc8f8b07c4d6010a42d778e605b2b
Anne <anne.freire@alu.ufc.br>
2026-03-31 17:53:49 +0000

    Atividade "tv"

---
+++ fub/tv/go/draft.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+func main() {
+    var valor float64
+    var parcelas int
+    fmt.Scan(&valor, &parcelas)
+
+    var juros float64
+
+    if parcelas == 1 {
+        juros = 0
+    } else {
+        juros = float64(parcelas-1) * 0.05
+    }
+
+    total := valor * (1 + juros)
+    parcela := total / float64(parcelas)
+
+    fmt.Printf("%.2f\n", parcela)
+    fmt.Printf("%.2f\n", total)
+
+
+}
