103a171b66250d304bbb9d241de2ba3fb3f5bec6
Anne <anne.freire@alu.ufc.br>
2026-04-08 12:29:41 +0000

    Atividade "abs"

--- fub/abs/go/draft.go
+++ fub/abs/go/draft.go
@@ -1,5 +1,22 @@
-package main
+ package main
 import "fmt"
+
+func abs (x int) int {
+    if x < 0 {
+        return -x
+    }
+    return x
+}
+
 func main() {
-    fmt.Println("Hello, World!")
+    var n1, n2 int
+    fmt.Scan(&n1, &n2)
+
+    diferenca := n1 - n2
+    
+    reseultado := abs(diferenca)
+
+    fmt.Println(reseultado)
+
+
 }
