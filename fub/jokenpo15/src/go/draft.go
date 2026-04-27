5a346a21c965c3f52a730b05b035bba3f4f975c4
Anne <anne.freire@alu.ufc.br>
2026-04-27 13:02:25 +0000

    Ver se presta aq

---
+++ fub/jokenpo15/src/go/draft.go
@@ -0,0 +1,33 @@
+package main
+import "fmt"
+func main() {
+    // x := 0 // criou antes para n zerar o 0 sempre
+
+    // for { // loop infinitob 
+    //     fmt.Println(x)
+    //     x += 1
+    // }
+
+    // for { // so sai quando o número ficar maior que 10
+    //     x += 1
+    //     fmt.Println(x)
+    //     if x > 10 {
+    //         break
+    //     }
+    // }
+
+    // for { // so os números ímpares
+    //     x += 1
+    //     if x % 2 == 0 {
+    //         continue // volta pro começo
+    //     }
+    //     fmt.Println(x)
+    //     if x > 10 {
+    //         break
+    //     }
+    // }
+
+    for x := 0; x < 10; x += 1 { // o que faz antes de começar o laço, o que faz
+
+    }
+}
