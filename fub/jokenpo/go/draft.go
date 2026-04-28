09e4a52040549df9f2dcc1decab21c17cc0a0914
Anne <anne.freire@alu.ufc.br>
2026-04-28 13:26:39 +0000

    Atividadade "jokenpo"

--- fub/jokenpo/go/draft.go
+++ fub/jokenpo/go/draft.go
@@ -1,23 +1,21 @@
 package main
 import "fmt"
 func main() {
-    const PEDRA int = 0
-    const PAPEL int = 1
-    const TESOURA int = 2
 
-    var jog1, jog2 int
-    fmt.Println("Jog1: Digite 0(pedra), 1(papel), 2(tesoura):")
-    fmt.Scan(&jog1)
-    fmt.Println("Jog2: Digite 0(pedra), 1(papel), 2(tesoura):")
-    fmt.Scan(&jog2)
+
+    var jog1, jog2 string
+    fmt.Scan(&jog1, &jog2)
 
     if jog1 == jog2 {
-        fmt.Println("Empate")
-    } else if (jog1 == PEDRA && jog2 == TESOURA) ||
-        (jog1 == PAPEL && jog2 == PEDRA) ||
-        (jog1 == TESOURA && jog2 == PAPEL) {
-        fmt.Println("Jog1 ganhou")
+        fmt.Println("empate")
+    } else if jog1 == "R" && jog2 == "S" {
+        fmt.Println("jog1")
+    } else if jog1 == "S" && jog2 == "P" {
+        fmt.Println("jog1")
+    } else if jog1 == "P" && jog2 == "R" {
+        fmt.Println("jog1")
     } else {
-        fmt.Println("Jog2 ganhou")
+        fmt.Println("jog2")
     }
+
 }
