b4ed24706f4d339cf22f71d5d15b052d70ed16a3
Anne <anne.freire@alu.ufc.br>
2026-04-28 12:33:03 +0000

    Atividade "meio"

--- fub/meio/src/go/draft.go
+++ fub/meio/src/go/draft.go
@@ -1,5 +1,14 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var n1, n2, n3 int
+    fmt.Scan(&n1, &n2, &n3)
+
+    if (n1 > n2 && n1 < n3) || (n1 < n2 && n1 > n3) {
+        fmt.Println(n1)
+    } else if (n2 > n1 && n2 < n3) || (n2 < n1 && n2 > n3) {
+        fmt.Println(n2)
+    } else {
+        fmt.Println(n3)
+    }
 }
