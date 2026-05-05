740815b61d9eea3dc74735530db0ba52bdee1bc2
Anne <anne.freire@alu.ufc.br>
2026-05-05 17:19:46 +0000

    .

--- fub/bhaskara/src/go/draft.go
+++ fub/bhaskara/src/go/draft.go
@@ -1,5 +1,14 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var A, B, C int
+    fmt.Scan(&A, &B, &C)
+
+    delta := B * B - 4 * A *C
+
+    if delta {
+        fmt.Println
+    }
+
+    fmt.Println(delta)
 }
