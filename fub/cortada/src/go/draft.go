1378beca785f3cd1c8c2aff00437c7493bb9e2b1
Anne <anne.freire@alu.ufc.br>
2026-05-18 17:05:39 +0000

    Atividade "cortada"

--- fub/cortada/src/go/draft.go
+++ fub/cortada/src/go/draft.go
@@ -1,5 +1,19 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("Hello, World!")
+    var B, T int
+    fmt.Scan(&B)
+    fmt.Scan(&T)
+
+    soma := B + T
+
+    if soma > 160 {
+        fmt.Println(1)
+    } else if soma < 160 {
+        fmt.Println(2)
+    } else {
+        fmt.Println(0)
+    }
 }
